36dbb61cdbca50b042e17a6142d6596fcf9b6254
Varbie Sumido <fdc.varbs@gmail.com>
2024-05-23 13:00:17 +0800

    update sql

--- MYSQL/join_activity.sql
+++ MYSQL/join_activity.sql
@@ -4,3 +4,30 @@ SELECT * from employees;
 SELECT * FROM employees where last_name = "Piveteau";
 -- List employees ordered by hire date in ascending order
 SELECT * FROM employees order by hire_date asc;
+-- Count the number of employees
+SELECT COUNT(*) FROM employees;
+-- Retrieve a list of employees along with their department names
+SELECT 
+	*,
+    d.dept_name
+FROM 
+	employees e
+INNER JOIN
+	dept_emp de ON e.emp_no = de.emp_no
+INNER JOIN
+	departments d ON de.dept_no = d.dept_no;
+
+-- Get a list of employees and their titles, including those who do not have a title currently assigned
+SELECT
+	*,
+    t.title
+FROM
+	employees e
+LEFT JOIN
+	titles t ON e.emp_no = t.emp_no
+
+	
+	
+
+
+	

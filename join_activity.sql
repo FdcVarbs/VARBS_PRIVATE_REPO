f79d2d20181eb4dd4878d7e631915e509d838537
Varbie Sumido <fdc.varbs@gmail.com>
2024-05-23 11:30:32 +0800

    add sql changes

--- join_activity.sql
+++ join_activity.sql
@@ -2,3 +2,5 @@
 SELECT * from employees;
 -- Find employees with the last name "Piveteau"
 SELECT * FROM employees where last_name = "Piveteau";
+-- List employees ordered by hire date in ascending order
+SELECT * FROM employees order by hire_date asc;
